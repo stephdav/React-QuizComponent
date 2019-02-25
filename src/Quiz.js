4f7c75531127dad775796e1f54ccbff32cb75b35
stephdav <stephane.david@soprasteria.com>
2019-02-25 12:09:18 +0100

    Completed the module7

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,8 +11,8 @@ class Quiz extends Component {
   }
 
   showNextQuestion() {
-    this.setState((m)=>{
-      return {quiz_position:m.quiz_position+1}
+    this.setState((state)=>{
+      return {quiz_position: state.quiz_position+1}
     });
   }
 
